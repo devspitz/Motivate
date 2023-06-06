50715c3fa07094fb4e1fa91b130b6f5da78d2e4a
devspitz <83680872+devspitz@users.noreply.github.com>
2023-06-05 22:15:36 -0400

    Delete hmwk 64 directory

--- hmwk 64/myApp2.js
+++
@@ -1,8 +0,0 @@
-window.myApp = window.myApp || {};
-
-window.myApp.util= (function (theModule) {
-  'use strict';
-   theModule.isStringEqual = (stringA, stringB) =>  stringA.toUpperCase() === stringB.toUpperCase();
-  
-    return theModule;
-  }(window.myApp.util || {});
